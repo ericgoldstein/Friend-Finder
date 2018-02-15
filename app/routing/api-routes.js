5d2ade4048d510e223bf7f281a4c26b6eaa5183e
ericgoldstein <bergerd306@aol.com>
2018-02-14 21:28:08 -0700

    readme

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,6 +3,7 @@ var friends = require('../data/friends.js');
 module.exports = function (app) {
   // //api path to get the friends data, responds with a json object (an array of friends). Activated on both html pages with blue API Link
   app.get('/api/friends', function (req,res) {
+
       res.json(friends);
   });
 
